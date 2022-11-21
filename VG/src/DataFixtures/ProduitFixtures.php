6aa5764aa992c09d1cbf1063f4c265c007e4000c
fcaumartin <fcaumartin@gmail.com>
2022-11-21 14:24:02 +0100

    add fixture

--- VG/src/DataFixtures/ProduitFixtures.php
+++ VG/src/DataFixtures/ProduitFixtures.php
@@ -10,12 +10,20 @@ class ProduitFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $produit = new Produit();
-        $produit->setLibelle("Super produit");
-        $produit->setPrix(2358.44);
+        $produit1 = new Produit();
+        $produit1->setLibelle("Guitare");
+        $produit1->setPrix(111.11);
+        $manager->persist($produit1);
 
+        $produit2 = new Produit();
+        $produit2->setLibelle("Piano");
+        $produit2->setPrix(222.22);
+        $manager->persist($produit2);
 
-        $manager->persist($produit);
+        $produit3 = new Produit();
+        $produit3->setLibelle("Batterie");
+        $produit3->setPrix(333.33);
+        $manager->persist($produit3);
 
         $manager->flush();
     }
